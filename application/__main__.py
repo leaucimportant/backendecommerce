d9b36fcdee7fd7556851fc67126bdafdc7266572
thomasNL <G1tTN!38L@b>
2023-11-28 20:12:15 +0100

    Test de la base de données avec Python

---
+++ application/__main__.py
@@ -0,0 +1,31 @@
+import dotenv
+import os
+import mysql.connector
+
+dotenv.load_dotenv()
+
+username = os.environ["MYSQL_USER"]
+password = os.environ["MYSQL_PASSWORD"]
+
+if os.environ["DATABASE_USE_ROOT"] == "true":
+    username = "root"
+    password = os.environ["SQL_ROOT_PASSWORD"]
+
+mydb = mysql.connector.connect(
+    host=os.environ["DATABASE_URL"],
+    user=username,
+    password=password,
+    database=os.environ["DATABASE_NAME"]
+)
+
+print(mydb)
+print(mydb.database)
+print(mydb.user)
+
+cursor = mydb.cursor()
+cursor.execute("SHOW TABLES;")
+for res in cursor:
+    print(res)
+
+cursor.close()
+mydb.close()
