b9b0b64b965f99622e384a930ed3c7ec68b2456c
thomasNL <G1tTN!38L@b>
2023-12-04 20:11:16 +0100

    Ajout des cross_origin

--- application/api/articles.py
+++ application/api/articles.py
@@ -2,6 +2,7 @@ from typing import Final
 
 from application.database import DataBase
 from flask import Blueprint, make_response, jsonify
+from flask_cors import cross_origin
 
 blueprint_articles: Final[Blueprint] = Blueprint(name="read_paint_pot_bp", import_name=__name__)
 
@@ -9,12 +10,14 @@ __all__ = ["blueprint_articles"]
 
 
 @blueprint_articles.route("/")
+@cross_origin()
 def read_articles():
     db_inst: DataBase = DataBase.get_instance()
     return make_response(jsonify(db_inst.get_all_article()), 200)
 
 
 @blueprint_articles.route("/<id>")
+@cross_origin()
 def read_article_id(id):
     db_inst: DataBase = DataBase.get_instance()
     return make_response(jsonify(db_inst.get_all_article_by_id(id)), 200)
